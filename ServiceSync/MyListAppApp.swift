0fd8620b18e41464bd7ffe13c1badc3ca5921a18
ariel513 <astvishor@gmail.com>
2024-11-05 18:01:17 -0600

    Created the ability to upload posts

---
+++ ServiceSync/MyListAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyListAppApp.swift
+//  MyListApp
+//
+//  Created by admin on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
